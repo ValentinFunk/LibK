af95e7802c9866fc8d1a723c49ebf087ae1fc050
Valentin Funk <funk.valentin@gmail.com>
2014-08-03 15:17:59 +0200

    Add possibility to fetch class resolving errors

--- lua/libk/server/sv_libk_config.lua
+++ lua/libk/server/sv_libk_config.lua
@@ -1,6 +1,6 @@
 LibK.SQL = {}
 --SQL Configuration
-LibK.SQL.UseMysql = false			--Set to true to use MSQL, false will use SQLite(sv.db)
+LibK.SQL.UseMysql = true			--Set to true to use MSQL, false will use SQLite(sv.db)
 --MySQL Settings
 LibK.SQL.Host = "localhost"
 LibK.SQL.Port = 3306
