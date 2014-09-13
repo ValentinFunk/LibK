d07711f3d0130bdf45c2977f6f6c396c235c9843
Valentin Funk <funk.valentin@gmail.com>
2014-09-13 18:05:35 +0200

    Added addon requirements
    Allows you to have an addon load only after another has loaded/fully
    updated

--- lua/libk/shared/sh_addonloader.lua
+++ lua/libk/shared/sh_addonloader.lua
@@ -51,6 +51,85 @@ local function loadAddon( addonTable )
 	KLog( 4, LibK.consoleHeader( 80, "=", name .. " by ".. author .. " loaded" ) .. "\n" )
 end
 
+local addonsInitialized = {}
+local waitingPromises = {}
+
+function LibK.ResetAddonsCache( )
+	addonsInitialized = {}
+end
+
+local function WhenAddonsLoaded( addonsRequired )
+	local def = Deferred()
+	
+	--Check if requirements are already loaded
+	local addonMissing = false
+	for _, addonName in pairs( addonsRequired ) do
+		if not table.HasValue( addonsInitialized, addonName ) then
+			addonMissing = true
+		end
+	end
+	if not addonMissing then
+		return Promise.Resolve( )
+	end
+	
+	--Queue for when requirements are loaded
+	table.insert( waitingPromises, {
+		addonsRequired = addonsRequired,
+		promise = def
+	} )
+	
+	return def:Promise( )
+end
+LibK.WhenAddonsLoaded = WhenAddonsLoaded
+
+local function onAddonInitialized( name )
+	table.insert( addonsInitialized, name )
+	for k, v in pairs( waitingPromises ) do
+		local addonMissing = false
+		for _, addonName in pairs( v.addonsRequired ) do
+			if not table.HasValue( addonsInitialized, addonName ) then
+				addonMissing = true
+			end
+		end
+		if not addonMissing then
+			waitingPromises[k] = nil
+			v.promise:Resolve( )
+		end
+	end
+end
+
+function onAddonInitializationFailed( name )
+	for k, v in pairs( waitingPromises ) do
+		if table.HasValue( v.addonsRequired, name ) then
+			v.promise:Reject( -1, "Parent addon " .. name .. " failed to initialize" )
+		end
+	end
+end
+
+local function doLoadAddon( addonTable )
+	local promise = Promise.Resolve( )
+	
+	if addonTable.requires then
+		promise = WhenAddonsLoaded( addonTable.requires )
+		if promise._state != "done" then
+			KLogf( 4, "Addon %s will be loaded once requirements (%s) are loaded", addonTable.addonName, table.concat( addonTable.requires, "," ) )
+		end
+	end
+	
+	promise:Done( function( )
+		if addonTable.loadAfterGamemode and not GAMEMODE then
+			hook.Add( "InitPostEntity", "LibKInit" .. addonTable.addonName, function( )
+				loadAddon( addonTable )
+				onAddonInitialized( addonTable.addonName )
+			end )
+			KLog( 4, LibK.consoleHeader( 80, "*", "Addon ".. name .. " will be loaded after gamemode init" ) )
+		else
+			loadAddon( addonTable )
+			onAddonInitialized( addonTable.addonName )
+		end
+	end )
+end
+
 function LibK.InitializeAddon( addonTable )
 	local luaroot = addonTable.luaroot
 	local name = addonTable.addonName
@@ -64,23 +143,19 @@ function LibK.InitializeAddon( addonTable )
 	table.insert( LibK.addonsLoaded, addonTable )
 
 	if SERVER then
-		if not LibK.updateAddon( addonTable ) then
+		LibK.updateAddon( addonTable )
+		:Fail( function( err )
+			ErrorNoHalt( err )
 			KLogf( 1, "[%s] Update Failed, aborting load!", addonTable.addonName )
-			return
-		end 
-	end
-	if addonTable.loadAfterGamemode and not GAMEMODE then
-		hook.Add( "InitPostEntity", "LibKInit" .. name, function( )
-			loadAddon( addonTable )
 		end )
-		KLog( 4, LibK.consoleHeader( 80, "*", "Addon ".. name .. " will be loaded after gamemode init" ) )
-	else
-		loadAddon( addonTable )
-	end
-	
-	--AddCSLuaFile Stuff
-	if SERVER then
+		:Done( function( )
+			KLogf( 4, "[%s] Update finished", addonTable.addonName )
+			doLoadAddon( addonTable )
+		end )
+		--AddCSLuaFile Stuff
 		addCSLua( luaroot .. "/client" )
 		addCSLua( luaroot .. "/shared" )
+	else
+		doLoadAddon( addonTable )
 	end
 end
