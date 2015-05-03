c75f1bb82404ea4a7f57e88924aa00e2139c7bf0
Valentin Funk <funk.valentin@gmail.com>
2015-05-03 14:39:37 +0200

    Load GLib only if no other version installed

--- lua/libk/shared/sh_thirdparty.lua
+++ lua/libk/shared/sh_thirdparty.lua
@@ -128,8 +128,13 @@ function LibK.loadThirdparty( tableName, author, virtualLua, virtualMain, mainFi
 end
 
 --GLib created by !cake, used with permission.
-LibK.loadThirdparty( "GLib", "!cake", "libk/3rdparty", "glib", "glib.lua" )
-
+if file.Exists( "autorun/glib.lua", "LUA" ) then 
+	include( "autorun/glib.lua" )
+	LibK.GLib = GLib
+else
+	LibK.loadThirdparty( "GLib", "!cake", "libk/3rdparty", "glib", "glib.lua" )
+end
+	
 --Gooey by !cake
 --LibK.loadThirdparty( "Gooey", "!cake", "libk/3rdparty", "gooey", "gooey.lua" )
 
