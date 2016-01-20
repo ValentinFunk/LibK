fe845d15fbc26ee1a42c971cceb1937d6736b671
Valentin Funk <funk.valentin@gmail.com>
2016-01-20 14:44:40 +0100

    Common pitfall in default config

--- lua/libk/server/sv_libk_config.lua
+++ lua/libk/server/sv_libk_config.lua
@@ -2,8 +2,8 @@ LibK.SQL = {}
 --SQL Configuration
 LibK.SQL.UseMysql = false --Set to true to use MSQL, false will use SQLite(sv.db)
 --MySQL Settings
-LibK.SQL.Host = "localhost"
+LibK.SQL.Host = "127.0.0.1"
 LibK.SQL.Port = 3306
 LibK.SQL.User = "root"
 LibK.SQL.Password = ""
-LibK.SQL.Database = "surfhd"
+LibK.SQL.Database = "dbname"
