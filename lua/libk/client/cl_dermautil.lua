0d1c7eab1592dfe2de0ecd01332db02effbdc1b5
Valentin Funk <funk.valentin@gmail.com>
2017-05-08 11:07:33 +0200

    Added LibK.DermaInherits

---
+++ lua/libk/client/cl_dermautil.lua
@@ -0,0 +1,35 @@
+local function getControlName(dermaInstance)
+    return table.KeyFromValue(derma.GetControlList(), dermaInstance)
+end
+
+--[[
+    Somehow gmod fucks up the last entry and we would end up in and
+    infine loop. A quirk is that the class name doesn't match the panel anymore
+]]--
+local function endOfChain(dermaInstance)
+    local controlName = getControlName(dermaInstance)
+    return derma.Controls[controlName].ClassName != controlName
+end
+
+--[[
+    Returns if aControlName inherits from controlName.
+    e.g. LibK.DermaInherits()
+]]--
+function LibK.DermaInherits(aControlName, controlName)
+    local parentName = derma.Controls[aControlName].BaseClass
+    local parent = derma.Controls[parentName]
+    while parent and not endOfChain() do
+        if parent == controlName then
+            return true
+        end
+
+        parentName = parent.BaseClass
+        if not parentName then
+            break
+        end
+        
+        parent = derma.Controls[parentName]
+    end
+    
+    return false
+end
