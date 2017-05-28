cbb8b2fa7ee304d4cc28f614f1d31f9fcf1459bf
Valentin Funk <funk.valentin@gmail.com>
2017-05-28 13:09:33 +0200

    fix(versioning): Persist version to database instead of FS Closes #9

--- lua/libk/server/sv_versioning.lua
+++ lua/libk/server/sv_versioning.lua
@@ -1,4 +1,4 @@
-/*
+--[[
 	LibK addon versioning:
 	Semver is used. LibK compares the addon's version to the version of the current installed version.
 	When the minor version is changed libk looks for an update script.
@@ -11,18 +11,56 @@
 
 	Limitations: Do not have mroe than 50 versions in between updates.
 	Patch versions are ignored, upgrade scripts should always have a patch of 0
-*/
+]]--
 
-function LibK.storeAddonVersion( addonTable, version )
-	if not file.IsDir( "libk", "DATA" ) then
-		file.CreateDir( "libk" )
-	end
-	file.Write( "libk/" .. util.CRC( addonTable.addonName ) .. ".txt", tostring( version ) )
+local UpdateDB = LibK.getDatabaseConnection( LibK.SQL, "Update" )
+
+local function createVersionTable()
+	return UpdateDB.DoQuery([[
+		CREATE TABLE IF NOT EXISTS libk_meta_version (
+			addon VARCHAR(255) NOT NULL PRIMARY KEY,
+			version VARCHAR(255) NOT NULL
+		)
+	]])
+end
+
+local function readVersionFromDB(addonName)
+	return UpdateDB.DoQuery("SELECT version FROM libk_meta_version WHERE addon = " .. UpdateDB.SQLStr(addonName))
+	:Then(function(rows) 
+		if not rows or not rows[1] then
+			return
+		end
+
+		return rows[1].version
+	end)
+end
+
+local function writeVersionToDB(addonName, version)
+	addonName = UpdateDB.SQLStr( addonName )
+	version = UpdateDB.SQLStr( version )
+	return UpdateDB.DoQuery( Format(
+		"REPLACE INTO libk_meta_version (addon, version) VALUES (%s, %s)",
+		addonName,
+		version
+	) )
 end
 
 function LibK.getStoredAddonVersion( addonTable )
-	local version = file.Read( "libk/" .. util.CRC( addonTable.addonName ) .. ".txt" )
-	return LibK.version( version or '0.0.0' )
+	return UpdateDB.ConnectionPromise
+	:Then( createVersionTable )
+	:Then( function( ) 
+		return readVersionFromDB( addonTable.addonName ) 
+	end )
+	:Then(function( version )
+		return LibK.version( version or '0.0.0' )
+	end)
+end
+
+function LibK.storeAddonVersion( addonTable, version )
+	return UpdateDB.ConnectionPromise
+	:Then(function()
+		return writeVersionToDB(addonTable.addonName, version)
+	end)
 end
 
 function LibK.performUpdate( addonTable, version )
@@ -39,58 +77,54 @@ function LibK.performUpdate( addonTable, version )
 	end
 
 	return err
-	:Done( function( )
+	:Then( function( )
 		KLogf( 5, "\tUpdated to %s", version )
-		LibK.storeAddonVersion( addonTable, version )
+		return LibK.storeAddonVersion( addonTable, version )
 	end )
 end
 
 function LibK.updateAddon( addonTable )
-	local version = LibK.getStoredAddonVersion( addonTable )
 	local newVersion = LibK.version( addonTable.version or '0.0.0' )
-	KLogf( 4, "Compare %s: %s to %s", addonTable.addonName, version, newVersion )
+	return LibK.getStoredAddonVersion( addonTable )
+	:Then(function( version ) 
+		KLogf( 4, "Compare %s: %s to %s", addonTable.addonName, version, newVersion )
+		if version >= newVersion then
+			KLogf( 4, "\t Up to date, nothing to do" )
+			return Promise.Resolve( )
+		end
 
-	if version >= newVersion then
-		KLogf( 4, "\t Up to date, nothing to do" )
-		return Promise.Resolve( )
-	end
+		local head = Format( "Updating %s: %s - %s", addonTable.addonName, version, newVersion )
+		KLog( 4, LibK.consoleHeader( 80, "=", head ) )
 
-	local head = Format( "Updating %s: %s - %s", addonTable.addonName, version, newVersion )
-	KLog( 4, LibK.consoleHeader( 80, "=", head ) )
+		local chainedPromise = Promise.Resolve( )
+		local currentVersion = version
+		for major = version.major, newVersion.major do
+			for minor = 0, 50 do
+				local versionToCheck = LibK.version( string.format( "%i.%i.0", major, minor ) )
+				if versionToCheck > newVersion then
+					break
+				end
 
-	local chainedPromise = Promise.Resolve( )
+				if versionToCheck <= currentVersion then
+					continue
+				end
 
-	local currentVersion = version
-	for major = version.major, newVersion.major do
-		for minor = 0, 50 do
-			local versionToCheck = LibK.version( string.format( "%i.%i.0", major, minor ) )
-			if versionToCheck > newVersion then
-				break
-			end
-
-			if versionToCheck <= currentVersion then
-				continue
-			end
-
-			local upgradeScriptName = addonTable.luaroot .. "/updatescripts/" .. tostring( versionToCheck ) .. ".lua"
-			if file.Exists( upgradeScriptName, "LUA" ) then
-				chainedPromise = chainedPromise:Then( function( )
-					KLogf( 5, "\tRunning update %s - %s", currentVersion, versionToCheck )
-					return LibK.performUpdate( addonTable, versionToCheck )
-				end )
-				currentVersion = versionToCheck
+				local upgradeScriptName = addonTable.luaroot .. "/updatescripts/" .. tostring( versionToCheck ) .. ".lua"
+				if file.Exists( upgradeScriptName, "LUA" ) then
+					chainedPromise = chainedPromise:Then( function( )
+						KLogf( 5, "\tRunning update %s - %s", currentVersion, versionToCheck )
+						return LibK.performUpdate( addonTable, versionToCheck )
+					end )
+					currentVersion = versionToCheck
+				end
 			end
 		end
-	end
 
-	chainedPromise:Done( function()
-		--if #promises == 0 then
-		--	KLog( 4, " -> Nothing to do" )
-			LibK.storeAddonVersion( addonTable, newVersion )
-		--end
+		return chainedPromise
+	end)
+	:Done( function()
+		LibK.storeAddonVersion( addonTable, newVersion )
 		local head = Format( "All Done", addonTable.addonName, version, newVersion )
 		KLog( 4, LibK.consoleHeader( 80, "=", head ) )
 	end )
-
-	return chainedPromise
 end
