b2c019c7ce9bd09e7571e62952ae55fd3166cec4
Valentin Funk <funk.valentin@gmail.com>
2018-02-04 21:03:05 +0100

    feat: add DComboBoxHack, a modal compatible DComboBox

--- lua/libk/client/cl_DMenuHack.lua
+++ lua/libk/client/cl_DMenuHack.lua
@@ -48,3 +48,66 @@ function DermaMenuHack(parent)
 
     return menu
 end
+
+local PANEL = {}
+
+function PANEL:OpenMenu( pControlOpener )
+
+	if ( pControlOpener && pControlOpener == self.TextEntry ) then
+		return
+	end
+
+	-- Don't do anything if there aren't any options..
+	if ( #self.Choices == 0 ) then return end
+
+	-- If the menu still exists and hasn't been deleted
+	-- then just close it and don't open a new one.
+	if ( IsValid( self.Menu ) ) then
+		CloseDermaMenus()
+	end
+
+	self.Menu = DermaMenuHack( self )
+
+	if ( self:GetSortItems() ) then
+		local sorted = {}
+		for k, v in pairs( self.Choices ) do
+			local val = tostring( v ) --tonumber( v ) || v -- This would make nicer number sorting, but SortedPairsByMemberValue doesn't seem to like number-string mixing
+			if ( string.len( val ) > 1 && !tonumber( val ) && val:StartWith( "#" ) ) then val = language.GetPhrase( val:sub( 2 ) ) end
+			table.insert( sorted, { id = k, data = v, label = val } )
+		end
+		for k, v in SortedPairsByMemberValue( sorted, "label" ) do
+			self.Menu:AddOption( v.data, function() self:ChooseOption( v.data, v.id ) end )
+		end
+	else
+		for k, v in pairs( self.Choices ) do
+			self.Menu:AddOption( v, function() self:ChooseOption( v, k ) end )
+		end
+	end
+
+	local x, y = self:LocalToScreen( 0, self:GetTall() )
+
+	self.Menu:SetMinimumWidth( self:GetWide() )
+	self.Menu:OpenForModal( x, y )
+end
+
+function PANEL:ChooseOption( value, index )
+
+	if IsValid( self.Menu ) then
+		CloseDermaMenus()
+	end
+
+	self:SetText( value )
+
+	-- This should really be the here, but it is too late now and convar changes are handled differently by different child elements
+	--self:ConVarChanged( self.Data[ index ] )
+
+	self.selected = index
+	self:OnSelect( index, value, self.Data[ index ] )
+
+end
+
+function PANEL:CloseMenu()
+    CloseDermaMenus()
+end
+
+derma.DefineControl( "DComboBoxHack", "", PANEL, "DComboBox" )
